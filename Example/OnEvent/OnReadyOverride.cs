807dfe6588f10b7dc04bca12adc006d0711531cd
Shockelite <shockelite@hotmail.com>
2025-05-24 14:31:10 -0700

    Example override of event.

---
+++ Example/OnEvent/OnReadyOverride.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace Discord.OnEvent {
+
+    /// <inheritdoc cref="BaseSocketClient.Ready"/>
+    public class OnReadyOverride : OnReady {
+
+        public OnReadyOverride(EventHandler eventHandler) : base(eventHandler) { }
+
+        /// <inheritdoc cref="BaseSocketClient.Ready"/>
+        override protected Task Event() {
+            base.Event();
+            Log("This is added by override.");
+            return Task.CompletedTask;
+        }
+
+    }
+
+}
